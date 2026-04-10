5f93af70ccee13ad76b3c1fb7ac08eb31ae16151
Theu13 <matheuslimabatista30@gmail.com>
2026-04-10 17:34:33 -0300

    Challenge 2 lvl2 completed

--- src/main/kotlin/level_2/FindBiggerSmaller.kt
+++ src/main/kotlin/level_2/FindBiggerSmaller.kt
@@ -1,9 +1,37 @@
 /*
-Challenge: .
-Description: .
-Concepts used: .
+Challenge: Find the bigger and the smaller number of a sequence.
+Description: input numbers and find bigger and smaller.
+Concepts used: fun, list, manipulation list, for.
 Date: april/2026
 */
 
 package level_2
+
+fun main(args: Array<String>) {
+
+    println("Write a list of numbers (ex: 5 3 6 2): ")
+    val input = readLine()!!
+
+    val numbers = input.split(" ").map { it.toInt() }
+
+    findBiggerSmaller(numbers)
+
+}
+
+fun findBiggerSmaller(list: List<Int>) {
+
+    var bigger = 0
+    var smaller = 0
+
+    //find bigger
+    for (i in list.indices) {
+        if (list[i] >= bigger) {
+            bigger = list[i]
+        } else {
+            smaller = list[i]
+        }
+    }
+
+    println("The Bigger number is $bigger and the smaller number is $smaller")
+}
 
